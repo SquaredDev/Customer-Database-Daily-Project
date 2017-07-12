a7deba366cac581e6b28ffd8b10a07c31c391231
James <boydston.j@gmail.com>
2017-07-12 15:08:45 -0700

    js working

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Customer-Database-Daily-Project
+
+You'll need to create your main.js and styles.css file and link them up properly. Once you do that, you'll need to use the object customers that is in the customers.js file to display the people on the page.
+
+You should do your best to match the mockup as perfect as possible. The only thing you can omit is the blurred out SSN unless you attempt the Hard Mode listed below.
+
+Hard Mode  
+
+You'll notice in the mockup, their SSN is listed. You should display this on the page as well, but have it be blurred out by default. Then on hover, allow the user to see it.
--- main.js
+++ main.js
@@ -1,4 +1,65 @@
-
-let body = document.querySelector('body')
+// const customers ={
+//    "results":[
+//       {
+//          "gender":"male",
+//          "name":{
+//             "title":"mr",
+//             "first":"curtis",
+//             "last":"ryan"
+//          },
+//          "location":{
+//             "street":"8073 rolling green rd",
+//             "city":"south valley",
+//             "state":"missouri",
+//             "postcode":99356
+//          },
+//          "email":"curtis.ryan@example.com",
+//          "login":{
+//             "username":"beautifulmouse544",
+//             "password":"beardog",
+//             "salt":"kL9djbKk",
+//             "md5":"67bedf8307958366eb547d6bc8f23a9a",
+//             "sha1":"33f493d4301964dff559850470758e8f4fa78c13",
+//             "sha256":"4a9e0d3fd2cafc6f6a1fc05cd6b35ceb0cfe08fa2b9abc07f3bfad5c977be86c"
+//          },
+//          "dob":"1967-10-29 09:16:09",
+//          "registered":"2007-01-12 19:26:30",
+//          "phone":"(518)-664-5821",
+//          "cell":"(385)-917-9080",
+//          "id":{
+//             "name":"SSN",
+//             "value":"544-97-7715"
+//          },
+//          "picture":{
+//             "large":"https://randomuser.me/api/portraits/men/0.jpg",
+//             "medium":"https://randomuser.me/api/portraits/med/men/0.jpg",
+//             "thumbnail":"https://randomuser.me/api/portraits/thumb/men/0.jpg"
+//          },
+//          "nat":"US"
+//       }
+// "info":{
+//    "seed":"b98b46987e52ab98",
+//    "results":12,
+//    "page":1,
+//    "version":"1.1"
+// }
+// };
 
 let htmlString = ''
+
+htmlString += `<div class='profile'>`
+
+customers.results.map(function(item) {
+    htmlString += `<img class="profilePic" src="${item.picture.large}">
+    <hr>
+    <p class="profileName">${item.name.first} ${item.name.last}</p>
+    <email class="profileEmail"><p>${item.email}</p></email>
+    <address class="profileAddress"><p>${item.location.street}<br>${item.location.city}, ${item.location.state} ${item.location.postcode}<br><span class="profilePhone">${item.phone}</span></p></address>
+    <p class="ssn">${item.id.value}</p>
+    `
+})
+
+
+htmlString += `</div><br>`
+
+document.querySelector('#directory').innerHTML = htmlString
