81cf19a116ee4918add30e830cf8939d41a19bf9
James <boydston.j@gmail.com>
2017-07-12 16:00:22 -0700

    4oclock

--- main.js
+++ main.js
@@ -50,16 +50,15 @@ let htmlString = ''
 htmlString += `<div class='profile'>`
 
 customers.results.map(function(item) {
-    htmlString += `<img class="profilePic" src="${item.picture.large}">
-    <hr>
+    htmlString += `<div class="profileWrap"><img class="profilePic" src="${item.picture.large}">
     <p class="profileName">${item.name.first} ${item.name.last}</p>
     <email class="profileEmail"><p>${item.email}</p></email>
     <address class="profileAddress"><p>${item.location.street}<br>${item.location.city}, ${item.location.state} ${item.location.postcode}<br><span class="profilePhone">${item.phone}</span></p></address>
-    <p class="ssn">${item.id.value}</p>
+    <p class="ssn">${item.id.value}</p></div>
     `
 })
 
 
-htmlString += `</div><br>`
+htmlString += `</div>`
 
 document.querySelector('#directory').innerHTML = htmlString
